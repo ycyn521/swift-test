4f71d716b68d382d902bd999605d212ecf195ac4
ycyn521 <ycyn521@qq.com>
2015-04-29 17:44:05 +0800

    first commit

---
+++ MyFirstSwift.playground/section-1.swift
@@ -0,0 +1,27 @@
+	// Playground - noun: a place where people can play
+
+import UIKit
+
+  // Int Double Float String  首字母都大写
+    
+    let float_a = 0.2
+    let bignum = 1_0000_0000
+    
+    let num_a:Float = 1
+    let num_b:Int = 3
+    let a:Int = 3
+    let b:Double = 0.1415926
+    
+    var pi:Double = Double(a)+b
+    
+    println(pi)
+    
+    
+    let 姓名 = "杨超"
+    
+    姓名 + "，你好"
+    
+    
+    
+    
+
